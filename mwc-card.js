27aabd08996fd18a1389f4012dde7314094c7786
Andrew Silluron <asilluron@users.noreply.github.com>
2019-05-02 19:46:33 -0700

    Wrong named import for lit-element

--- mwc-card.js
+++ mwc-card.js
@@ -14,7 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import {LitElement, html, css} from 'lit-element/lit-element.js';
+import {LitElement, html} from 'lit-element';
 import {classMap} from 'lit-html/directives/class-map.js';
 import {style} from './mwc-card-css.js';
 
